5f2a80072e4558f780b80a24dc83de4224f8e71d
guoyanshuai <847807873@qq.com>
2018-10-09 18:33:42 +0800

    修改新增文件ginfrompost.go文件

--- webgin/ginformpost.go
+++ webgin/ginformpost.go
@@ -20,13 +20,21 @@ func main() {
 	})
 
 	r.POST("/post", func(context *gin.Context) {
-		id :=context.Query("id")
-		page :=context.DefaultQuery("page","0")
-		name :=context.PostForm("name")
+		id :=context.Query("id") //通过url获取
+		page :=context.DefaultQuery("page","0") //通过url获取
+		name :=context.PostForm("name") //通过 from-data类型设置获取
 		message :=context.PostForm("message")
 
 		context.String(http.StatusOK,"id %s ,page %s , name %s , message %s ",id,page,name,message)
 
+	})
+
+	r.POST("/postmap", func(context *gin.Context) {
+		//请求的url
+		//http://127.0.0.1:7895/postmap?names[first]=thinkerou&names[second]=tianou&ids[a]=1234&ids[b]=hello
+		ids :=context.QueryMap("ids")
+		names :=context.QueryMap("names")
+		context.String(http.StatusOK,"ids: %v; names: %v", ids, names)
 	})
 
 	r.Run(":7895")
